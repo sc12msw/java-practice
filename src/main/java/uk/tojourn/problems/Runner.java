b47626fe894d7d7ea8617eaf404e0d98310dc2c3
Matt Watson <matthew.watson2@skybettingandgaming.com>
2022-08-08 14:09:47 +0100

    Mikey is bae

--- src/main/java/uk/tojourn/problems/Runner.java
+++ src/main/java/uk/tojourn/problems/Runner.java
@@ -6,6 +6,7 @@ import org.springframework.context.event.EventListener;
 public class Runner {
     @EventListener(ApplicationReadyEvent.class)
     public void run() {
-
+        WordValuer valuer = new WordValuer();
+        System.out.println(valuer.getValueOf("bob"));
     }
 }
--- src/main/java/uk/tojourn/problems/WordValuer.java
+++ src/main/java/uk/tojourn/problems/WordValuer.java
@@ -1,9 +1,7 @@
 package uk.tojourn.problems;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.nio.CharBuffer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,18 +10,20 @@ import java.util.regex.Pattern;
 public class WordValuer {
     private final Pattern pattern = Pattern.compile("[a-z]+");
     public int getValueOf(String word) {
-
-        Matcher matcher = pattern.matcher(word);
+        String lowercase = word.toLowerCase();
+        Matcher matcher = pattern.matcher(lowercase);
         if (matcher.matches()) {
             //We want an index starting at one, so we are reducing the number by the starting character in ascii a=97 so -96 = 1
-            return word.toLowerCase().chars().reduce(0, (subtotal, letter) ->
-                    subtotal + valueFor((char)letter)
-            );
+            return lowercase.chars().reduce(0, WordValuer::reduceLetterByValue);
         } else return 0;
 
     }
-    private int valueFor(char letter){
+    private static int valueFor(char letter){
         return letter - 96;
+    }
+
+    private static int reduceLetterByValue(int accumulator, int letter){
+        return accumulator + valueFor((char)letter);
     }
 
 
